54add82e5af66c67f732f8338a168b90c38a428e
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-10 12:04:21 -0500

    defaults to not display forms, can display forms by clicking

--- frontend/components/show_item.jsx
+++ frontend/components/show_item.jsx
@@ -6,10 +6,18 @@ class ShowItem extends React.Component {
     super(props);
     this.handleLike = this.handleLike.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.toggleBody = this.toggleBody.bind(this);
+    this.toggleComment = this.toggleComment.bind(this);
     this.state = {
-      className: "show_item"
+      className: "show_item",
+      bodydisplay: true,
+      commentDisplay: false
     };
   }
+
+
+
+
 
   handleLike(value){
     if(this.props.currentUser){
@@ -26,10 +34,21 @@ class ShowItem extends React.Component {
       this.props.history.push("/");
     }
   }
+
+  toggleBody(){
+    this.setState({bodydisplay: !this.state.bodydisplay});
+  }
+  toggleComment(){
+    this.setState({commentDisplay: !this.state.commentDisplay});
+  }
   render(){
     let likes = Object.values(this.props.likes).filter(like => (like.item_id === this.props.item.id));
     let likeNumber = likes.reduce((accumulator, currentValue) => (accumulator + currentValue.value), 0);
-    
+    let bodydisplay = {true: "body", false: "hide"};
+    let updatedisplay = {false: "update_form", true: "hide"};
+    let deleteDisplay = {false: "deleteButton", true: "hide"};
+    let commentButtonDisplay = {false: "comment_button", true: "hide"};
+    let commentFormDisplay = {true: "comment_form", false: "hide"};
     return(
       <div className={this.state.className}>
         <section className="sidebar">
@@ -38,16 +57,17 @@ class ShowItem extends React.Component {
           <section className="down_arrow" onClick = {this.handleLike(-1)}/>
         </section>
         <section className="main">
-          <h1 className="body">{this.props.item.body}</h1>
-          <h3 className="username">{this.props.user.username}</h3>
-          <section className = "update_form">
-            <ItemForm userId={this.props.user.id} body={this.props.item.body} title={this.props.item.title} content_type={this.props.item.content_type}
+          <h1 className={bodydisplay[this.state.bodydisplay]} onClick={this.toggleBody}>{this.props.item.body}</h1>
+          <section className = {updatedisplay[this.state.bodydisplay]}>
+            <ItemForm userId={this.props.user.id} body={this.props.item.body} title={this.props.item.title} content_type={this.props.item.content_type} className={this.props.item.content_type}
               parent_id={this.props.item.parent_id} method={this.props.updateItem} id={this.props.item.id}/>
           </section>
-          <section className="comment_form">
+          <button onClick={this.handleDelete} value="Delete" className={deleteDisplay[this.state.bodydisplay]}>Delete</button>
+          <h3 className="username">{this.props.user.username}</h3>
+          <section className={commentFormDisplay[this.state.commentDisplay]}>
             <textarea></textarea>
           </section>
-          <button onClick={this.handleDelete} value="Delete">Delete</button>
+          <h2 className={commentButtonDisplay[this.state.commentDisplay] } onClick={this.toggleComment}>add a comment</h2>
         </section>
 
       </div>
