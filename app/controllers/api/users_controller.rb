e0b49d1a32ec46dde15ccb5f91c770656e61f0ee
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-12 09:35:32 -0500

    fixed posting questions for first time user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,9 +2,9 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+
       sign_in(@user)
-      render json: {username: @user.username}
+      render json: {username: @user.username, id: @user.id}
     else
       render json: @user.errors.full_messages, status: 422
     end
