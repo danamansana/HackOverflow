c7cac77ed52c3e8469a1f5c1773dbc8f1115a711
danamansana <dan.s.appel@gmail.com>
2018-01-24 10:37:45 -0500

    nav

--- frontend/components/nav.jsx
+++ frontend/components/nav.jsx
@@ -40,6 +40,7 @@ class Nav extends React.Component {
     } else {
       return (
         <div className="nav">
+          
           <h1 className="logo"><Link to="/"><i className="fa fa-stack-overflow" aria-hidden="true"></i><h1 className="hack">hack</h1>Overflow</Link></h1>
           <div className="link_holder">
 
